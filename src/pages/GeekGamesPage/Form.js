9e7aa1755c65b8ad970e84eca4ea6bfe90db8668
R I T E S H <81812754+ritessshhh@users.noreply.github.com>
2024-01-17 02:34:42 +0530

    Final Commit

--- src/pages/GeekGamesPage/Form.js
+++ src/pages/GeekGamesPage/Form.js
@@ -15,27 +15,12 @@ const TallyFormComponent = () => {
   }, []);
 
   return (
-    <div className="">
-      <iframe
-        data-tally-src="https://tally.so/embed/n0OeMy?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-        loading="lazy"
-        width="100%"
-        height="1250"
-        frameBorder="0"
-        marginHeight="0"
-        marginWidth="0"
-        title="GEEK GAMES REGISTRATION"
-      />
+    <div className="tally-form">
+      <iframe data-tally-src="https://tally.so/embed/mK1v9V?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="2816" frameborder="0" marginheight="0" marginwidth="0" title="Registration"></iframe>
       <Helmet>
         <script>
           {`
-            if (typeof window.Tally !== 'undefined') {
-              window.Tally.loadEmbeds();
-            } else {
-              document.querySelectorAll('iframe[data-tally-src]:not([src])').forEach((element) => {
-                element.src = element.dataset.tallySrc;
-              });
-            }
+            var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}
           `}
         </script>
       </Helmet>
